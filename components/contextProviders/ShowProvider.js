bd8e6e051d73eb17917631c34f3e345cce0dd44c
SurriyaLuavan <surriyaluavan@gmail.com>
2023-03-23 15:21:40 -0300

    Add context provider for current bookmarks

--- components/contextProviders/ShowProvider.js
+++ components/contextProviders/ShowProvider.js
@@ -1,5 +1,35 @@
-const ShowProvider = () => {
-  return <div>Enter</div>;
-};
+import React from "react";
+import { createContext, useContext, useState } from "react";
+import Layout from "./Layout";
 
-export default ShowProvider;
+const ShowContext = createContext();
+
+export default function ShowProvider({ children, data }) {
+  const [bookmark, setBookmark] = useState(
+    data.map((item) => {
+      return { title: item.title, bookmarkStatus: item.isBookmarked };
+    })
+  );
+
+  function handleBookmark(title) {
+    setBookmark((prev) => {
+      return prev.map((item) => {
+        if (item.title === title)
+          return { ...item, bookmarkStatus: !item.bookmarkStatus };
+        else {
+          return item;
+        }
+      });
+    });
+  }
+
+  return (
+    <ShowContext.Provider
+      value={{ bookmark, onBookmarked: handleBookmark, data: data }}
+    >
+      {children}
+    </ShowContext.Provider>
+  );
+}
+
+export const useShow = () => useContext(ShowContext);
--- components/layout/SearchResults.js
+++ components/layout/SearchResults.js
@@ -1,4 +1,10 @@
-const SearchResults = () => {
+import { useRouter } from "next/router";
+
+const SearchResults = ({ searchValue }) => {
+  const router = useRouter();
+  const { bookmark } = useShow();
+  const regex = new RegExp(`\\b${searchValue}\\w*\\b`);
+  let collection;
   return <div>Enter</div>;
 };
 
