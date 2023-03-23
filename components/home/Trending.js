e640fa0cb43b2b90872c1e9fd42706470197e861
SurriyaLuavan <surriyaluavan@gmail.com>
2023-03-23 15:49:39 -0300

    Update trending component structure

--- components/home/Trending.js
+++ components/home/Trending.js
@@ -1,5 +1,26 @@
+import { useShow } from "../contextProviders/ShowProvider";
+import ScrollContainer from "react-indiana-drag-scroll";
+import uuid from "react-uuid";
+import ShowCard from "../ShowCard";
+
 const Trending = () => {
-  return <div></div>;
+  const { data } = useShow();
+  const trendingCollection = data.filter((item) => item.isTrending);
+
+  return (
+    <article>
+      <h1 className="padding-inline fs-l-primary-heading fw-light">Trending</h1>
+      <ScrollContainer
+        vertical={false}
+        hideScrollbars={false}
+        className="scroll-container trendingShowContainer | padding-inline padding-block-top padding-block-bottom"
+      >
+        {trendingCollection.map((item) => (
+          <ShowCard show={item} key={uuid()} container="trending" />
+        ))}
+      </ScrollContainer>
+    </article>
+  );
 };
 
 export default Trending;
