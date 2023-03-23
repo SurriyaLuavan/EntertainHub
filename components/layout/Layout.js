e54dad49028db85388588432851a65e13f9165e5
SurriyaLuavan <surriyaluavan@gmail.com>
2023-03-23 14:55:20 -0300

    Add Layout functionality and CSS module file

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,5 +1,26 @@
-const Layout = () => {
-  return <div></div>;
+import { useState } from "react";
+import NavBar from "./NavBar";
+import SearchResults from "./SearchResults";
+import SearchBar from "./SearchBar";
+import styles from "/styles/Layout.module.css";
+
+const Layout = ({ children }) => {
+  const [searchValue, setSearchValue] = useState("");
+  return (
+    <main className={styles.layoutContainer}>
+      <NavBar />
+      <SearchBar
+        searchValue={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+        onBack={() => setSearchValue("")}
+      />
+      {searchValue === "" ? (
+        children
+      ) : (
+        <SearchResults searchValue={searchValue} />
+      )}
+    </main>
+  );
 };
 
 export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -12,12 +12,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/assets/favicon.png" />
       </Head>
-      <main>
-        <Layout>
-          <Trending />
-          <Recommendations />
-        </Layout>
-      </main>
+      <Layout>
+        <Trending />
+        <Recommendations />
+      </Layout>
     </>
   );
 }
