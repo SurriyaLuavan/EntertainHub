d018ab96cb99813d90c5c61f1dbdcdd7cdcc4a16
SurriyaLuavan <surriyaluavan@gmail.com>
2023-03-23 15:32:21 -0300

    Add search results container functionality

--- components/layout/SearchResults.js
+++ components/layout/SearchResults.js
@@ -1,11 +1,50 @@
 import { useRouter } from "next/router";
+import { useShow } from "../contextProviders/ShowProvider";
+import ShowCard from "../ShowCard";
+import uuid from "react-uuid";
 
 const SearchResults = ({ searchValue }) => {
   const router = useRouter();
-  const { bookmark } = useShow();
+  const { bookmark, data } = useShow();
   const regex = new RegExp(`\\b${searchValue}\\w*\\b`);
   let collection;
-  return <div>Enter</div>;
+
+  switch (router.pathname) {
+    case "/":
+      collection = data;
+      break;
+    case "/movies":
+      collection = data.filter((item) => item.category === "Movie");
+      break;
+    case "/tv-series":
+      collection = data.filter((item) => item.category === "TV Series");
+      break;
+    case "/bookmarks":
+      collection = data.filter((item, index) => bookmark[index].bookmarkStatus);
+      break;
+    default:
+      collection = data;
+  }
+
+  const searchFilter = collection.filter((item) =>
+    regex.test(item.title.toLowerCase())
+  );
+  return (
+    <section className="showListContainer">
+      <h2 className="padding-inline fs-l-primary-heading fw-light">
+        Found {searchFilter.length} results for &lsquo;{searchValue}&rsquo;
+      </h2>
+      <div
+        className={`showCardContainer ${
+          searchFilter.length <= 2 && "alignSearch"
+        } | padding-inline padding-block-top padding-block-bottom`}
+      >
+        {/* {searchFilter.map((item) => (
+          <ShowCard show={item} key={uuid()} />
+        ))} */}
+      </div>
+    </section>
+  );
 };
 
 export default SearchResults;
