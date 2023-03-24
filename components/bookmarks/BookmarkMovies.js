c73a360b103a4c5e2c11037100badede14849ed2
SurriyaLuavan <surriyaluavan@gmail.com>
2023-03-24 09:12:18 -0300

    Update bookmark movies component

--- components/bookmarks/BookmarkMovies.js
+++ components/bookmarks/BookmarkMovies.js
@@ -1,5 +1,45 @@
+import { useShow } from "../contextProviders/ShowProvider";
+import ShowCard from "../ShowCard";
+import uuid from "react-uuid";
+
 const BookmarkMovies = () => {
-  return <div>Enter</div>;
+  const { bookmark, data } = useShow();
+  const bookmarkedCollection = data.filter(
+    (item, index) => bookmark[index].bookmarkStatus
+  );
+  const bookmarkedMovies = bookmarkedCollection.filter(
+    (item) => item.category === "Movie"
+  );
+  return (
+    <article>
+      <h1 className="padding-inline fs-l-primary-heading fw-light">
+        Bookmarked Movies
+      </h1>
+      {bookmarkedMovies.length > 0 ? (
+        <div
+          className={`showCardContainer ${
+            bookmarkedMovies.length <= 3 && "alignCard"
+          } | padding-inline padding-block-top padding-block-bottom`}
+        >
+          {bookmarkedMovies.map((item) => (
+            <ShowCard show={item} key={uuid()} />
+          ))}
+        </div>
+      ) : (
+        <p
+          className="fs-m-primary-heading fw-light"
+          style={{
+            display: "grid",
+            placeContent: "center",
+            placeItems: "center",
+            gridAutoRows: "15vh",
+          }}
+        >
+          No bookmarked movies
+        </p>
+      )}
+    </article>
+  );
 };
 
 export default BookmarkMovies;
