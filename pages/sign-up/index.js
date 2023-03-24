cf10b4e7e76e44f57b9f50e317d6c23275b8ba8b
SurriyaLuavan <surriyaluavan@gmail.com>
2023-03-24 08:55:49 -0300

    Add sigup page structure

--- pages/sign-up/index.js
+++ pages/sign-up/index.js
@@ -1,6 +1,30 @@
 import Head from "next/head";
+import Link from "next/link";
+import styles from "/styles/AuthPage.module.css";
+import { useState } from "react";
 
 export default function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeat, setRepeat] = useState("");
+  const [valid, setValid] = useState({
+    email: true,
+    password: true,
+    repeat: true,
+  });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    let currentStatus = {};
+
+    Object.keys(valid).forEach((item) => {
+      if (item === "") {
+        currentStatus = { ...currentStatus, item: false };
+      }
+    });
+  }
+
   return (
     <>
       <Head>
@@ -9,7 +33,98 @@ export default function Signup() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/assets/favicon.png" />
       </Head>
-      <main></main>
+      <main className={styles.loginContainer}>
+        <Link className={styles.logo} href="/" data-tab="home">
+          <svg width="33" height="27" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="m26.463.408 3.2 6.4h-4.8l-3.2-6.4h-3.2l3.2 6.4h-4.8l-3.2-6.4h-3.2l3.2 6.4h-4.8l-3.2-6.4h-1.6a3.186 3.186 0 0 0-3.184 3.2l-.016 19.2a3.2 3.2 0 0 0 3.2 3.2h25.6a3.2 3.2 0 0 0 3.2-3.2V.408h-6.4Z"
+              fill="#FC4747"
+            />
+          </svg>
+        </Link>
+        <form
+          action="GET"
+          className={`${styles.formContainer} bg-primary-600`}
+          onSubmit={handleSubmit}
+        >
+          <h1 className={`${styles.fsHeading} fw-light`}>Sign Up</h1>
+          <div className={styles.inputFieldContainer}>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              onFocus={() => setValid({ email: true, password: true })}
+              className={`${styles.inputField} | ${styles.fsInput} ${
+                !valid && styles.errorField
+              } bg-primary-600 text-neutral-100 fw-light`}
+              placeholder="Email address"
+            />
+            {!valid.email && (
+              <span
+                className={` ${styles.errorMessage} ${styles.fsError} text-accent fw-light`}
+              >
+                Invalid email ID
+              </span>
+            )}
+          </div>
+          <div className={styles.inputFieldContainer}>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              onFocus={() => setValid({ email: true, password: true })}
+              className={`${styles.inputField}  | ${styles.fsInput} ${
+                !valid.password && styles.errorField
+              } bg-primary-600 text-neutral-100 fw-light`}
+              placeholder="Password"
+            />
+            {!valid.email && (
+              <span
+                className={` ${styles.errorMessage} ${styles.fsError} text-accent fw-light`}
+              >
+                Invalid email ID
+              </span>
+            )}
+          </div>
+          <div className={styles.inputFieldContainer}>
+            <input
+              type="password"
+              name="password-repeat"
+              id="password-repeat"
+              value={repeat}
+              onChange={(e) => setPassword(e.target.value)}
+              onFocus={() => setValid({ email: true, password: true })}
+              className={`${styles.inputField}  | ${styles.fsInput} ${
+                !valid.password && styles.errorField
+              } bg-primary-600 text-neutral-100 fw-light`}
+              placeholder="Repeat password"
+            />
+            {!valid.email && (
+              <span
+                className={` ${styles.errorMessage} ${styles.fsError} text-accent fw-light`}
+              >
+                Invalid email ID
+              </span>
+            )}
+          </div>
+          <button
+            type="submit"
+            className={`${styles.submitButton} | ${styles.fsInput} fw-light`}
+          >
+            Create an account
+          </button>
+          <p className={`${styles.fsInput} ${styles.para} fw-light`}>
+            Already have an account?{" "}
+            <Link href="/login" className="text-accent">
+              Login
+            </Link>
+          </p>
+        </form>
+      </main>
     </>
   );
 }
